b4bd5a1a0e1d6915dca6d2552a14c0d9af5ebdf3
Michael Zupecki <michaelzupecki@gmail.com>
2018-02-22 11:37:40 +1100

    Initial commit, basic blocking nailed out

---
+++ blackjack.py
@@ -0,0 +1,35 @@
+# Initial primary code for command line Blackjack - might split into separate files if necessary
+# A Game has Players, a Dealer and a Table
+# A Table has a Deck and state of Cards
+# A Player has Cards and set of actions
+# A Dealer has Cards and set of actions
+# Unique Dealer object?
+
+
+class Table:
+
+    # code
+
+class Dealer:
+
+    # code
+
+class Player:
+
+    # code
+
+class Deck:
+
+    # code
+
+class Card:
+
+    # code
+
+class Game:
+
+    # code
+
+# Game logic
+
+##
