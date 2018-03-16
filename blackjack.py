c08d4373b9a221dda872e62ddb6f79b65ce5d715
Michael Zupecki <michaelzupecki@gmail.com>
2018-03-16 11:01:46 +1100

    created Dealer subclass of Player - a type of Player with special attributes and methods; has exclusive access to the Deck (owns it) as well as the ability to deal a card to a Player. Prepred game.players tuple to be any size (x,) for future feature of adding variable amount of Player's to game. Added Card.art and Card.pos for future art assets to be added, as well as x,y position for drawing to screen. Built method Player.show_hand() to automatically take care of showing a Player's hand. Built Dealer deal_card() method, which takes care of popping a card from the deck and passing it to a stipulated Player, decrementing the deck count (via deck.pop_card()) and incrementing the Player's hand count. Created misc num_to_word(num) function to replace ints with their worded-string counterpart (eg. 1 = One, 2 = Two etc). Created and then deprecated game.give_card which was superceded, in same edit, by Dealer.deal_card(). Wrote a bunch of tests to check objects states and ensure cards were popped and transferred to Player's correctly.

--- blackjack.py
+++ blackjack.py
@@ -4,50 +4,75 @@
 # A Deck has Cards or is Empty
 from random import shuffle
 
-class Player():
+# misc global functions - probably best in own file
 
-  # name can be either Player's real name, or default "Player 1"/"Dealer" which are automatically assigned
+# convert int num to word-string version
+def num_to_word(num):
+  ref = {0:"Zero",1:"One",2:"Two",3:"Three",4:"Four",5:"Five",
+  6:"Six",7:"Seven",8:"Eight",9:"Nine",10:"Ten"}
+
+  return ref[num]
+
+class Player():
 
   def __init__(self, name):
       self.name = name
       self.hand = Hand()
 
   def show_hand(self):
-      for card in hand:
-          print(card)
+    for key, value in self.hand.cards.items():
+      for card in value:
+        print(card)
 
   def hand_value(self):
     self.hand
+
+# Dealer subcalass of Player with special features
+class Dealer(Player):
+
+  def __init__(self, name, deck):
+    Player.__init__(self, name)
+    self.deck = deck
+
+  def deal_card(self, player):
+    card = self.deck.pop_card()
+
+    if(card.title != "Ace"):
+      cardType = "fixedCards"
+    else:
+      cardType = "aces"
+
+    player.hand.cards[cardType].append(card)
+    player.hand.count += 1
+    print("{} - popped from deck and given to {}".format(card, player.name))
+
+  def shuffle_deck(self):
+    return None
 
 class Hand():
   def __init__(self):
       self.cards = {'fixedCards':[], 'aces': []}
       self.value = 0
+      self.count = 0
 
   # iterate through all cards and sum hand value
   def calculate_value(self):
-      for key in self.cards:
-          for card in self.cards[key]:
-              value += card.value
+    for key in self.cards:
+      for card in self.cards[key]:
+        value += card.value
 
       # if resulting hand value over 21, and there is at least one ace
       if self.value > 21 and len(self.cards['aces']) > 0:
-          change_aces()
+        change_aces()
 
   # called if hand exceeds 21 and holding aces. Change aces to 1 until hand less than 21
   def change_aces(self):
-      for card in cards['aces']:
-          if card.value == 11:
-              card.value = 1
-              calculate_value()
-          if value <= 21:
-              break
-
-  # cycle through cards in Hand, call their string output
-  def show_hand(self):
-    for cardType in self.cards:
-      for card in cardType:
-        print(card)
+    for card in cards['aces']:
+      if card.value == 11:
+        card.value = 1
+        calculate_value()
+      if value <= 21:
+        break
             
 class Deck():
 
@@ -62,7 +87,8 @@ class Deck():
     for suit in suits:
       # standard cards
       for value in range(2,11):
-        self.cards.append(Card(suit, value, None))
+        title = num_to_word(value)
+        self.cards.append(Card(suit, value, title))
 
       # special cards
       for kind in kinds:
@@ -95,22 +121,22 @@ class Deck():
 class Card():
 
   # all cards have suit (clubs, spades, diamonds, hearts) and a value (number, or special class)
-  def __init__(self, suit, value, kind):
+  def __init__(self, suit, value, title):
     self.suit = suit
     self.value = value
-    self.kind = kind
+    self.title = title
+    # adding attributes for potential art to be added
+    self.art = None
+    self.pos = [None, None]
 
   # print card in correct format
   def __str__(self):
-    if(self.kind == None):
-      return "{} of {} - ({})".format(str(self.value), self.suit, self.value)
-    else:
-      return "{} of {} - ({})".format(self.kind, self.suit, self.value)
+    return "{} of {} - ({})".format(self.title, self.suit, self.value)
 
 class Game():
   def __init__(self):
     self.players = ()
-    self.deck = Deck()
+    self.dealer = None
     self.won = False
 
   def deal(self):
@@ -119,13 +145,21 @@ class Game():
     # if card is Ace, check Hand value
 
   def start(self):
+    return None
+
+  def setup(self):
     # get Player's name
     print("Welcome to Python Blackjack, also known colloquially as Twenty One!\nPlease enter your name:")
-    self.players = (Player("Dealer"), Player(input()))
+    name = input()
+    self.players = (Player(name),)
 
     # Deck
-    self.deck.build()
-    self.deck.shuffle()
+    deck = Deck()
+    deck.build()
+    deck.shuffle()
+
+    # create Dealer and give deck
+    self.dealer = Dealer("Dealer", deck)
 
   def check_win(self):
     return None
@@ -139,24 +173,41 @@ class Game():
     return None
     # code
 
+  # give card to Player, update Hand
+  def give_card(self, card, player):
+    if(card.title != "Ace"):
+      cardType = "fixedCards"
+    else:
+      cardType = "aces"
+
+    player.hand.cards[cardType].append(card)
+    player.hand.count += 1
 # Game logic
 
-# Build and shuffle deck - to go inside Game object in game.start()
+# Build game object, then set it up with Players, Dealer and Deck
 game = Game()
-game.start()
+game.setup()
 
 print("\n")
-game.deck.show_cards()
+# show Deck for testing
+game.dealer.deck.show_cards()
+print("\n{} cards in the deck.\n".format(game.dealer.deck.count))
 
-print("\n{} cards in the deck.\n".format(game.deck.count))
+# card 1
+game.dealer.deal_card(game.players[0])
 
-card = game.deck.pop_card()
-print("{} - popped from deck".format(card))
+# card 2
+game.dealer.deal_card(game.players[0])
 
-print("\n{} cards in the deck.\n".format(game.deck.count))
+# show hand
+print("\n{} holding {} cards in Hand:".format(game.players[0].name, str(game.players[0].hand.count)))
+game.players[0].show_hand()
+
+# report how many cards are in the deck
+print("\n{} cards in the deck.\n".format(game.dealer.deck.count))
 
 print("Player's name is {}".format(game.players[0].name))
-print("Player's name is {}".format(game.players[1].name))
+print("Player's name is {}".format(game.dealer.name))
 
 # Player enters name - how many players? **kwargs? (["Michael": 1, "Jason": 2"]) etc? Or **args? (["Michael, Jason"]) etc?
 # Cards created
